c5ceef4b37f35d5cdd8717e52d27e308d091f310
Adam Weil <adamjweil@gmail.com>
2018-02-21 17:29:28 -0500

    contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import ContactForm from './Contact/ContactForm';
+
 const Contact = (props) => (
   <div className="main-content about">
-          <h2>Contact</h2>
-
-        </div>
+        <h2>Contact</h2>
+      <ContactForm />
+  </div>
 );
 
 export default Contact;
