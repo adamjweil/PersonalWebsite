6aeda9c4d74ff93f12136ec62f6d969f952fb12d
Adam Weil <adamjweil@gmail.com>
2018-02-22 16:07:22 -0500

    formatting contact form

---
+++ src/Components/Contact/SelectDropDown.js
@@ -0,0 +1,16 @@
+<FormGroup
+  controlId="formControlsSelect" >
+  <ControlLabel>Topic:</ControlLabel>
+  <FormControl
+    componentClass="select"
+    placeholder="select">
+    <option value="Pls select your reason for contacting me from the below list...">Pls select your reason for contacting me from the below list...</option>
+    <option value="Question about your code">Question about your code</option>
+    <option value="Need a developer to help on an existing project">Looking for a developer</option>
+    <option value="Looking to discuss a potential project that you would be great for">Looking to discuss a potential project that you would be great for</option>
+    <option value="Want to get your opinion on an idea">Want to get your opinion on an idea</option>
+    <option value="General Inquiry">General Inquiry</option>
+    <option value="Other">Other</option>
+   </FormControl>
+  <FormControl.Feedback />
+</FormGroup>
