40d87578cc9d450441484543050c57db3e39f18b
Adam Weil <adamjweil@gmail.com>
2018-03-03 18:30:54 -0500

    making the photos higher quality

--- src/Components/Projects/Coinstache/CoinstachePhotos.js
+++ src/Components/Projects/Coinstache/CoinstachePhotos.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
 const slides = [{
-  imageUrl: "https://user-images.githubusercontent.com/25589910/36452888-2c7f27dc-1664-11e8-8a1c-c72dfae1e987.png",
+  imageUrl: "https://s3.amazonaws.com/aw-personal-website-images/Coin_MainPage.png",
   caption: "Main Page"
 }, {
-  imageUrl: "https://user-images.githubusercontent.com/25589910/36452959-660a725e-1664-11e8-87a1-7af8325fa510.png",
+  imageUrl: "https://s3.amazonaws.com/aw-personal-website-images/Coin_Dashboard.png",
   caption: "Dashboard"
 }, {
-  imageUrl: "https://user-images.githubusercontent.com/25589910/36453122-f9fe721c-1664-11e8-9f5e-fbad65b9048a.png",
+  imageUrl: "https://s3.amazonaws.com/aw-personal-website-images/Coin_BTCtop.png",
   caption: "Bitcoin Page (Top): Market price data and real-time tweets relating to Bitcoin"
 }, {
-  imageUrl: "https://user-images.githubusercontent.com/25589910/36453231-5cffa066-1665-11e8-92dc-93a52f8a3185.png",
+  imageUrl: "https://s3.amazonaws.com/aw-personal-website-images/Coin_BTCbottom.png",
   caption: "Bitcoin Page (Bottom): Real-time RSS feeds relating to Bitcoin"
 }, {
-  imageUrl: "https://user-images.githubusercontent.com/25589910/36453456-0c20480c-1666-11e8-83d8-8369104cfd98.png",
+  imageUrl: "https://s3.amazonaws.com/aw-personal-website-images/Coin_Wallets.png",
   caption: "Wallets Page: Shows current balance in USD, BTC, ETH, and LTC wallets, as well as most recent 25 transactions and the associated details"
 }, {
-  imageUrl: "https://user-images.githubusercontent.com/25589910/36453510-448bd7ce-1666-11e8-8e64-633fab9bfda5.png",
+  imageUrl: "https://s3.amazonaws.com/aw-personal-website-images/Coin_BuySell.png",
   caption: "Buying/Selling"
 }, {
-  imageUrl: "https://user-images.githubusercontent.com/25589910/36453545-624126d4-1666-11e8-9cf0-ddbeafd47740.png",
+  imageUrl: "https://s3.amazonaws.com/aw-personal-website-images/Coin_P2P.png",
   caption: "P2P Transfer Made Easy"
 }];
 
@@ -44,7 +44,7 @@ class CoinstachePhotos extends React.Component {
     return (
       <div className="coinPhotoContainer">
         <div className="coinMainPhoto">
-            <img src={this.state.mainPhotoURL} />
+            <img src={this.state.mainPhotoURL} style={{height:'450px',width:'300px;'}}/>
           <div className="coinBlurb">
             <p>{this.state.mainPhotoBlurb}</p>
           </div>
