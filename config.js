ed425184fc697e9d7f4c6aa0e239e09628547e7c
Adam Weil <adamjweil@gmail.com>
2018-03-09 16:08:07 -0500

    removing phone number from site

---
+++ config.js
@@ -0,0 +1,13 @@
+import source from './client_id.json'
+
+const user = 'awpersonalwebsite@gmail.com'
+const refreshToken = '1/M4k09IrsXqkiXeFYnY_kAzWYvpFwTl0zPOtO5ZX-skw'
+const accessToken = 'ya29.Glt1BWWBL7MI0I6Io_llEOuYCGAwGXO5cr3pzLzAzsXleuZv6wD9mjs6Yyo1_hlvIWFFCFzOhw-c5YJBwiardMlBCDHqyG-u2IyrA0iuRfR51W4jo2t1mUM-S5ms'
+
+export default {
+  user,
+  clientId: source.web.client_id,
+  clientSecret: source.web.client_secret,
+  refreshToken,
+  accessToken
+}
